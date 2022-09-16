12b678b8c67f65b1c0b0ca068d1a1547dc602133
youtube <youtube@mahesh-2.local>
2022-09-17 01:24:02 +0530

    Alpha

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'easy_arc_reactor.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
